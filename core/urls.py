8fc25423242101c8a3d6dfe5d9a2774d287558cd
jhonRR28 <rmales1728@hotmail.com>
2025-08-13 21:17:58 -0500

    segundo avance

---
+++ core/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls.conf import include
+
+from .views import HomeView, UserProductListView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+
+    path('users/', include('accounts.urls', namespace='users')),
+
+    path('', HomeView.as_view(), name='home'),
+    path('products/', UserProductListView.as_view(), name='product-list'),
+
+    path('marketplace/', include('marketplace.urls', namespace="marketplace")),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
---
+++ core/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+
+from marketplace.models import Product
+
+from django.core.paginator import Paginator
+
+from marketplace.forms import ProductModelForm
+
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        #Llama solo los productos que esten activos
+        products = Product.objects.filter(active=True)
+        form = ProductModelForm()
+
+        #Paginacion de vistas
+        digital_products_data = None
+
+        if products:
+            paginator = Paginator(products, 9)
+            page_number = request.GET.get('page')
+            digital_products_data = paginator.get_page(page_number)
+
+        context = {
+            'products':digital_products_data,
+            'form':form
+        }
+        return render(request, 'pages/index.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        products = Product.objects.filter(active=True)
+        
+        form = ProductModelForm()
+
+        if request.method == "POST":
+            form = ProductModelForm(request.POST, request.FILES)
+
+            if form.is_valid():
+                form.user = request.user
+                name = form.cleaned_data.get('name')
+                description = form.cleaned_data.get('description')
+                thumbnail = form.cleaned_data.get('thumbnail')
+                slug = form.cleaned_data.get('slug')
+                content_url = form.cleaned_data.get('content_url')
+                content_file = form.cleaned_data.get('content_file')
+                price = form.cleaned_data.get('price')
+                active = form.cleaned_data.get('active')
+
+                p, created = Product.objects.get_or_create(user=form.user,name=name,description=description, thumbnail=thumbnail, slug=slug, content_url=content_url, content_file=content_file,price=price, active=active)
+                p.save()
+                return redirect('home')
+
+
+
+
+        #Paginacion de vistas
+        digital_products_data = None
+        
+        if products:
+            paginator = Paginator(products, 9)
+            page_number = request.GET.get('page')
+            digital_products_data = paginator.get_page(page_number)
+
+        context = {
+            'products':digital_products_data,
+        }
+        return render(request, 'pages/index.html', context)
+    
+
+class UserProductListView(View):
+    def get(self, request, *args, **kwargs):
+
+        products = Product.objects.filter(user = self.request.user)
+
+        context = {
+            'products' : products
+        }
+        return render(request, 'pages/products/user_productlist.html', context)
+    
